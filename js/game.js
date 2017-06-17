a135cd9980a6650188c2426e56bdabf85c00eb95
AlexTheProgrammer <alexander.business.s@gmail.com>
2017-06-17 22:42:01 +0930

    initial commit.

---
+++ js/game.js
@@ -0,0 +1,11 @@
+function runGame() {
+
+div0 = document.getElementById("div-0") ;
+
+elem = document.createElement("img")
+div0.appendChild(elem);
+elem.src = "../resources/bunny.png";
+
+}
+
+window.onload = runGame;
